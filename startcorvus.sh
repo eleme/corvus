4daec50369250831a13879450d6dddb9d501c457
root <root@redis3proxy1-1.sde38.mycmdb.net>
2017-03-13 18:42:27 +0000

    initscript

--- startcorvus.sh
+++ startcorvus.sh
@@ -1,8 +1,8 @@
 killall -9 corvus
 
-./src/corvus -n 10.254.127.37:6379,10.254.127.48:6379,10.254.127.49:6379 -t 4 -b 6379 -L debug ./corvus.conf  1>/var/log/corvus.log 2>/var/log/corvus.err &
+/usr/bin/corvus -n 10.254.127.37:6379,10.254.127.48:6379,10.254.127.49:6379 -t 4 -b 6379 -L debug /etc/corvus.conf 1>/var/log/corvus.log 2>/var/log/corvus.err &
 CORVUSPID=$!
 echo Startted Corvus with PID=$CORVUSPID
 echo $CORVUSPID > /var/run/corvus.pid
-wait $CORVUSPID
-rm /var/run/corvus.pid
+ps aux |grep corvus
+
