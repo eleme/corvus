84df1b3d6fc11e57afd8b1a6ccd5db57b11f40c4
Tevin Zhang <mail2tevin@gmail.com>
2017-03-14 19:25:26 +0800

    Add support for SELECT command

--- src/command.h
+++ src/command.h
@@ -145,7 +145,7 @@
     HANDLER(PROXY,             EXTRA,    UNKNOWN) \
     HANDLER(SLOWLOG,           EXTRA,    UNKNOWN) \
     HANDLER(QUIT,              EXTRA,    UNKNOWN) \
-    HANDLER(SELECT,            UNIMPL,   UNKNOWN) \
+    HANDLER(SELECT,            EXTRA,    UNKNOWN) \
     HANDLER(TIME,              EXTRA,    UNKNOWN) \
     HANDLER(CONFIG,            EXTRA,    UNKNOWN)
 
